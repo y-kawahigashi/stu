009ea018a1abd106276dc66668aee2e1fcb167f2
'fujino' <'fko2447032@stu.o-hara.ac.jp'>
2025-04-09 13:57:58 +0900

    1

---
+++ src/dao/ClassNumDao.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.util.List;
+
+import bean.ClassNum;
+import bean.School;
+
+public class ClassNumDao extends Dao{
+
+	public ClassNum get(String classnum, School school
+			) throws Exception {
+
+	}
+
+	public List<String> filter(School school) throws Exception {
+
+	}
+
+	public boolean save(ClassNum classNum) throws Exception {
+
+	}
+
+	public boolean save(ClassNum classNum, String newClassNum) throws Exception {
+
+	}
+
+}
