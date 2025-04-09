7bce1f7aba1175954bf06a7d4d89a15170d90232
'fujino' <'fko2447032@stu.o-hara.ac.jp'>
2025-04-09 14:11:39 +0900

    1

--- src/scoremanager/main/StudentListAction.java
+++ src/scoremanager/main/StudentListAction.java
@@ -2,7 +2,9 @@ package scoremanager.main;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Teacher;
 import tool.Action;
 
 public class StudentListAction extends Action{
@@ -10,6 +12,8 @@ public class StudentListAction extends Action{
 	@Override
 	public void execute(HttpServletRequest request,HttpServletResponse response
 			) throws Exception {
-
+			HttpSession session = request.getSession();
+			Teacher teacher = (Teacher)session.getAttribute("user");
 	}
+
 }
