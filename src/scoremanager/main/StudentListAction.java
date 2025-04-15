dda0a34091ef6d964c16e873dfbbf2617fbb1985
'fujino' <'fko2447032@stu.o-hara.ac.jp'>
2025-04-15 12:39:38 +0900

    fujino

--- src/scoremanager/main/StudentListAction.java
+++ src/scoremanager/main/StudentListAction.java
@@ -52,19 +52,39 @@ public class StudentListAction extends Action{
 				entYearSet.add(i);
 			}
 
+			//DBからデータ取得
+			//ログインユーザーの学校コードをもとにクラス番号の一覧を取得
 			List<String> list = cNumDao.filter(teacher.getschool());
 
 			if (entYear != 0 && !classNum.equals("0")){
+				//入学年度とクラス番号を指定
 				students = sDao.filter(teacher.getschool(),entYear,classNum,isAttend);
 			} else if (entYear != 0 && classNum.equals("0")){
+				//入学年度のみ指定
 				students = sDao.filter(teacher.getschool(), entYear, isAttend);
 			} else if (entYear != 0 && classNum == null || entYear == 0 && classNum.equals("0")){
+				//指定なしの場合
+				//全学年情報を取得
 				students = sDao.filter(teacher.getschool(), isAttend);
 			} else {
 				errors.put("f1","クラスを指定する場合は入学年度も指定してください");
 				request.setAttribute("errors", errors);
+				//全学年情報を取得
 				students = sDao.filter(teacher.getschool(), isAttend);
 			}
+
+			request.setAttribute("f1", entYear);
+			request.setAttribute("f2", classNum);
+
+			if (isAttendStr != null) {
+				isAttend = true;
+				request.setAttribute("f3", isAttendStr);
+			}
+			request.setAttribute("students", students);
+			request.setAttribute("class_num_set", list);
+			request.setAttribute("ent_year_set", entYearSet);
+
+			request.getRequestDispatcher("student_list.jsp").forward(request, response);
 	}
 
 }
