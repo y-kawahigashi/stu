99899bb092883341e8c1c538a1cf59dbd9a331bf
'fujino' <'fko2447032@stu.o-hara.ac.jp'>
2025-04-15 11:22:00 +0900

    fujino

--- src/scoremanager/main/StudentListAction.java
+++ src/scoremanager/main/StudentListAction.java
@@ -1,10 +1,18 @@
 package scoremanager.main;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.Student;
 import bean.Teacher;
+import dao.ClassNumDao;
+import dao.StudentDao;
 import tool.Action;
 
 public class StudentListAction extends Action{
@@ -14,6 +22,21 @@ public class StudentListAction extends Action{
 			) throws Exception {
 			HttpSession session = request.getSession();
 			Teacher teacher = (Teacher)session.getAttribute("user");
+
+			String entYearStr = "";//入力された入学年度
+			String classNum = "";//入力されたクラス番号
+			String isAttendStr = "";//入力された在学フラグ
+			int entYear = 0;//入学年度
+			boolean isAttend = false;//在学フラグ
+			List<Student> students = null;//学生リスト
+			LocalDate todaysDate = LocalDate.now();//LocalDateインスタンスを取得
+			int year = todaysDate.getYear();//現在の年を取得
+			StudentDao sDao = new StudentDao();//学生Dao
+			ClassNumDao cNumDao = new ClassNumDao();//クラス番号Daoを初期化
+			Map<String,String> errors = new HashMap<>();//エラーメッセージ
+
+			//リクエストパラメータの取得
+			entYearStr = request
 	}
 
 }
--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -11,11 +11,27 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("/FrontController")
+@WebServlet("*.action")
 public class FrontController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response
 			)throws ServletException, IOException{
+		try{
+			//パスを取得
+			String path = request.getServletPath().substring(1);
+			//ファイル名を取得しクラス名に変換
+			String name = path.replace(".a","A").replace("/", ".");
+
+			System.out.println("★ servlet path -> " + request.getServletPath());
+			System.out.println("★ class name -> " + name);
+
+			Action action = (Action) Class.forName(name).getDeclaredConstructor().newInstance();
+
+			action.execute(request,response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.getRequestDispatcher("/error.jsp").forward(request, response);
+		}
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
